b83a16921c7a4ba6f6e38287b732017a30c8dfb0
阿田 <5219254+artian@user.noreply.gitee.com>
2020-03-16 17:09:24 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    Hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~common/stylus/variable.styl'
+#app
+  background: $color-background
+  color: $color-theme
+</style>
